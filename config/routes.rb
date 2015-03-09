4695d9458ff5ec492ddf7237e9ca42503f75dea4
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-03-09 21:58:59 +0900

    せっかくなので最初から。というファーストコミット

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :events
+
+  root to: 'welcome#index'
+  get '/auth/:provider/callback' => 'sessions#create'
+  get 'logout' => 'sessions#destroy', as: :logout
+end
+
