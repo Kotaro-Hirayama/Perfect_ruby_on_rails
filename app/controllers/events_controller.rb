be44624874aa11bb830f45ddc17d4bdddd7b0a62
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-03-11 07:21:43 +0900

    イベント編集・削除の追加 P202完了

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,10 @@
 class EventsController < ApplicationController
   before_action :authenticate
+
   def new
     @event = current_user.created_events.build
   end
+
   def create
     @event = current_user.created_events.build(event_params)
     if @event.save
@@ -14,6 +16,25 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = current_user.created_events.find(params[:id])
+  end
+
+  def update
+    @event = current_user.created_events.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event, notice: '更新しました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event = current_user.created_events.find(params[:id])
+    @event.destroy!
+    redirect_to root_path, notice: '削除しました'
   end
 
   private
