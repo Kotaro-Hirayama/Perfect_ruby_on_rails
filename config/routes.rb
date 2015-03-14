0ed0eaa5d93ef5d4d908ce973ffa02970e6d3e1a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-03-14 14:16:52 +0900

    退会用コントローラ、ビュー、ルーティングの作成 P218完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :events
-
   root to: 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#create'
   get 'logout' => 'sessions#destroy', as: :logout
+
+  resource :user do
+    get 'retire'
+  end
 
   resources :events do
     resources :tickets
